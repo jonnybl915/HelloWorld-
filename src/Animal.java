b1ca9bf98d147bbd7bd257cac927329c0ef14132
jonathandavidblack <jonnybl915@gmail.com>
2016-05-16 12:11:41 -0400

    add animal class

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jonathandavidblack on 5/16/16.
+ */
+public class Animal {
+    //variable declaration
+    String name;
+    String species;
+    double weight;
+    boolean isMamal;
+
+}
