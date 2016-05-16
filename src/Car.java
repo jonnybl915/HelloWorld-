7058445e91d8768422009e2ba6992c4c7037876c
jonathandavidblack <jonnybl915@gmail.com>
2016-05-16 12:12:14 -0400

    add Car and House classes

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jonathandavidblack on 5/16/16.
+ */
+public class Car {
+    String make;
+    String model;
+    int year;
+    boolean stillRuns;
+
+}
---
+++ src/House.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jonathandavidblack on 5/16/16.
+ */
+public class House {
+    int age;
+    String streetName;
+    double streetNumber;
+    boolean isSingleHouse;
+
+}
