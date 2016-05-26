5064d621f585f928157426c0cfa363519bbaa2b5
jonathandavidblack <jonnybl915@gmail.com>
2016-05-26 10:22:54 -0400

    add Exercise2

--- src/Email.java
+++ src/Email.java
@@ -31,6 +31,16 @@ public class Email {
         this.time =0;
     }
 
+    @Override
+    public String toString() {
+        return "Email{" +
+                "author=" + author +
+                ", subject='" + subject + '\'' +
+                ", body='" + body + '\'' +
+                ", destination='" + destination + '\'' +
+                ", time=" + time +
+                '}';
+    }
 
     public Person getAuthor() {
         return author;
